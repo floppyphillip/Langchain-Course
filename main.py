866e07be02224e3d1c5e87540d90a8f7c4bee221
floppyphillip <floppyphillip@yahoo.com>
2026-03-21 17:31:59 +0100

    Structured output introduction

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
 
 # from tavily import TavilyClient
 
 load_dotenv()
+
 
 # tavily = TavilyClient()
 
@@ -24,15 +27,30 @@ load_dotenv()
 #     print(f"Searching for {query}")
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
 # tools = [search]
-agent = create_agent(model=llm,tools=tools)
+agent = create_agent(model=llm,tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-search-agent!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the wheather in Tokyo")})
+    result = agent.invoke(
+                {
+                "messages":HumanMessage(
+                    content="Search for 3 job openings fo an ai engineer using langchain in the bay area on linkedin"
+                    )
+                }
+            )
     print(result)
 
 if __name__ == "__main__":
